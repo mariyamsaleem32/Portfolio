5653f045e9247795275dd1f662d01557bce80491
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2025-01-30 12:58:08 +0500

    added nav

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppLayout from './layout';
 import Dashboard from './dashboard';
-// import navApp from './navLayout';
+import navApp from './navLayout';
 
 const Index = () => {
-    return (
-        <div>
-<AppLayout><Dashboard/></AppLayout>
-        </div>
-    );
-}
+  const [isResponsive, setIsResponsive] = useState(false);
+
+  // Update the state when the window size changes
+  useEffect(() => {
+    const handleResize = () => {
+      setIsResponsive(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    
+    // Initial check
+    handleResize();
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <AppLayout>
+      {isResponsive ? <navApp /> : <Dashboard />}
+    </AppLayout>
+  );
+};
 
 export default Index;
